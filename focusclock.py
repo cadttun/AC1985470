f3fac21fc03614302dd8e02a4f2448296bc2d0a5
cadttun <132211871+cadttun@users.noreply.github.com>
2023-04-30 22:49:38 +0800

    Create focusclock.py

---
+++ focusclock.py
@@ -0,0 +1,15 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print('Time up!')
+
+# 调用函数设置25分钟的专注时间
+focus_timer(25)

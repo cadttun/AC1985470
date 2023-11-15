38015fb927398dfa0c9d7fef97f1944aa17046c8
cadttun <132211871+cadttun@users.noreply.github.com>
2023-11-15 22:46:21 +0800

    Update focusclock.py

--- focusclock.py
+++ focusclock.py
@@ -6,8 +6,8 @@ def focus_timer(minutes):
         mins, secs = divmod(seconds, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end="\r")
-        time.sleep(1)
-        seconds -= 1
+        time.sleep(2)
+        seconds -= 2
 
     print('Time up!')
 
